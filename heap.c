c8533371638c9af7ddcb10af23597ceb470a6144
Matias <matias32p@gmail.com>
2022-05-30 17:37:04 -0300

    Funciones crear y destruir

--- heap.c
+++ heap.c
@@ -10,3 +10,31 @@ struct heap {
     size_t cant;
     cmp_func_t cmp;
 };
+
+
+heap_t *heap_crear(cmp_func_t cmp) {
+    heap_t* heap = malloc(sizeof(heap_t));
+    if (heap == NULL) return NULL;
+
+    void* datos = malloc(sizeof(void*) * 64);
+    if (datos==NULL) {
+        free(heap);
+        return NULL;
+    }
+    heap->datos = datos;
+    heap->cant = 0;
+    heap->tam = 64;
+    heap->cmp = cmp;
+
+    return heap;
+}
+
+void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)) {
+    if (destruir_elemento != NULL) {
+        for (size_t i = 0; i<heap->cant; i++) {
+            destruir_elemento(heap->datos[i]);
+        }
+    }
+    free(heap->datos);
+    free(heap);
+}
